062bc229c698bad46a5636f0a460c7e5fccf38d4
David C. Manuelda <StormByte@gmail.com>
2026-09-04 21:56:42 +0200

    docs: namespace briefs on typedefs and item types

--- lib/public/StormByte/config/item/type.hxx
+++ lib/public/StormByte/config/item/type.hxx
@@ -1,30 +1,36 @@
 /*
- * Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
- *
- * This file is part of StormByte-Config.
- *
- * StormByte-Config is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License version 3
- * or later, as published by the Free Software Foundation.
- *
- * StormByte-Config is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with StormByte-Config. If not, see
- * <https://www.gnu.org/licenses/lgpl-3.0.html>.
- */
+* Copyright (C) 2024-2026 David C. Manuelda (StormBytePP)
+*
+* This file is part of StormByte-Config.
+*
+* StormByte-Config is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License version 3
+* or later, as published by the Free Software Foundation.
+*
+* StormByte-Config is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with StormByte-Config. If not, see
+* <https://www.gnu.org/licenses/lgpl-3.0.html>.
+*/
 
 #pragma once
+
 #include <StormByte/config/exception.hxx>
 #include <StormByte/config/visibility.h>
-#include <cstddef>
+
 #include <concepts>
+#include <cstddef>
 #include <string>
 #include <type_traits>
 #include <vector>
+
+/**
+ * @brief Configuration items (values, comments, groups, lists).
+ */
 namespace StormByte::Config::Item {
 	/**
 	 * @enum Type
@@ -37,7 +43,7 @@ namespace StormByte::Config::Item {
 		Double,     ///< Double-precision floating-point item.
 		Integer,    ///< Integer item.
 		String,     ///< String item.
-		Binary      ///< Binary data (stored as std::vector<std::byte>, text form is Base64 with b"..." prefix)
+		Binary      ///< Binary data (`std::vector<std::byte>`; text form is Base64 `b"..."`)
 	};
 
 	/**
@@ -47,9 +53,9 @@ namespace StormByte::Config::Item {
 	 */
 	static constexpr std::string TypeToString(const Type& t) noexcept {
 		switch(t) {
-			case Type::String: 		return "String";
+			case Type::String:		return "String";
 			case Type::Integer:		return "Integer";
-			case Type::Double: 		return "Double";
+			case Type::Double:		return "Double";
 			case Type::Comment:		return "Comment";
 			case Type::Bool:		return "Bool";
 			case Type::Container:	return "Container";
@@ -60,41 +66,41 @@ namespace StormByte::Config::Item {
 
 	/**
 	 * @enum CommentType
-	 * @brief Comment type
+	 * @brief Comment syntax used in the text document.
 	 */
 	enum class STORMBYTE_CONFIG_PUBLIC CommentType: char {
-		SingleLineBash = 2, 	///< Single line comment starting with # (bash style)
-		SingleLineC = 5,		///< Single line comment starting with // (C++ style)
-		MultiLineC = 8			///< Multi line comment starting with /* and ending with */ (C/C++ style)
+		SingleLineBash = 2,		///< `#` until end of line
+		SingleLineC = 5,		///< `//` until end of line
+		MultiLineC = 8			///< `/*` … `*/`
 	};
 
 	/**
-	 * Gets strings from Type
-	 * @param t type to convert
-	 * @return string
+	 * @brief Converts a CommentType to a human-readable string.
+	 * @param t Comment type to convert.
+	 * @return String representation.
 	 */
 	constexpr STORMBYTE_CONFIG_PUBLIC std::string TypeToString(const CommentType& t) noexcept {
 		switch(t) {
-			case CommentType::SingleLineBash: 	return "Bash like single line comment";
-			case CommentType::SingleLineC: 		return "C++ like single line comment";
-			case CommentType::MultiLineC: 		return "C/C++ like multi line comment";
-			default: 							return "Unknown";
+			case CommentType::SingleLineBash:	return "Bash like single line comment";
+			case CommentType::SingleLineC:		return "C++ like single line comment";
+			case CommentType::MultiLineC:		return "C/C++ like multi line comment";
+			default:							return "Unknown";
 		}
 	}
 
 	/**
 	 * @enum ContainerType
-	 * @brief Container type
+	 * @brief Group (`{}`) or list (`[]`).
 	 */
 	enum class STORMBYTE_CONFIG_PUBLIC ContainerType: char {
-		Group, 	///< Group of items
-		List	///< List of items
+		Group,	///< Group of named items
+		List	///< List of unnamed items
 	};
 
 	/**
-	 * Gets strings from Type
-	 * @param t type to convert
-	 * @return string
+	 * @brief Converts a ContainerType to a string.
+	 * @param t Container type to convert.
+	 * @return String representation.
 	 */
 	constexpr STORMBYTE_CONFIG_PUBLIC std::string TypeToString(const ContainerType& t) noexcept {
 		switch(t) {
@@ -105,18 +111,22 @@ namespace StormByte::Config::Item {
 	}
 
 	/**
-	 * Gets Type from start character
-	 * @param start start character
-	 * @return Type Container type
+	 * @brief Resolves a container type from its opening character.
+	 * @param start `{` or `[`.
+	 * @return Container type.
+	 * @throw Exception if the character is not a container opener.
 	 */
 	constexpr STORMBYTE_CONFIG_PUBLIC ContainerType TypeFromStartCharacter(const char& start) {
 		switch(start) {
-			case '{': 			return ContainerType::Group;
-			case '[': 			return ContainerType::List;
-			default: 			throw Exception("Unknown start character " + std::string(1, start) + " for container");
+			case '{':	return ContainerType::Group;
+			case '[':	return ContainerType::List;
+			default:	throw Exception("Unknown start character " + std::string(1, start) + " for container");
 		}
 	}
 
+	/**
+	 * @brief Value types allowed in `Item::Value<T>`.
+	 */
 	template<typename T>
 	concept AllowedValueType =
 		std::is_same_v<T, int> ||
@@ -124,5 +134,4 @@ namespace StormByte::Config::Item {
 		std::is_same_v<T, bool> ||
 		std::is_same_v<T, std::string> ||
 		std::is_same_v<T, std::vector<std::byte>>;
-
 }
